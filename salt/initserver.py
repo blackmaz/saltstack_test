9d6fbbebd95bc9e91c6ca45cf077c76dc804c348
CHO, Sungsic <sungsic.cho@gmail.com>
2017-01-22 12:52:51 +0900

    initserver add priv_key path

--- salt/initserver.py
+++ salt/initserver.py
@@ -1,17 +1,20 @@
 import salt.client.ssh.client
 
+#ssh_key='/home/ubuntu/aws/test_key_pair'
+ssh_key='/home/shungsic/.ssh/id_rsa'
+
 s = salt.client.ssh.client.SSHClient()
-r = s.cmd('*','test.ping')
+r = s.cmd('*','test.ping',ssh_priv=ssh_key,ssh_identities_only=True)
 for host in r.keys():
   if r[host]['retcode'] == 10:
-    ret = s.cmd(host, 'sudo cat /etc/*release|grep DISTRIB_ID',raw_shell=True)
+    ret = s.cmd(host, 'sudo cat /etc/*release|grep DISTRIB_ID',raw_shell=True,ssh_priv=ssh_key,ssh_identities_only=True)
     if ret[host]['stdout'] == 'DISTRIB_ID=Ubuntu\n':
-      ret = s.cmd(host,'sudo apt-get -y install python', raw_shell=True)
+      ret = s.cmd(host,'sudo apt-get -y install python', raw_shell=True,ssh_priv=ssh_key,ssh_identities_only=True)
 
 r = s.cmd('*','test.ping')
 for host in r.keys():
   if r[host]['retcode'] == 0:
-    ret = s.cmd(host,'state.apply',['salt_minion'])
+    ret = s.cmd(host,'state.apply',['salt_minion'],ssh_priv=ssh_key,ssh_identities_only=True)
 
 import salt.config
 import salt.wheel
@@ -19,4 +22,5 @@ o = salt.config.master_config('/etc/salt/master')
 w = salt.wheel.WheelClient(o)
 
 m = w.cmd('key.list',['all'])
-w.cmd('key.accept',m['minions_pre'])
+if len(m['minions_pre']) > 0:
+  w.cmd('key.accept',m['minions_pre'])
