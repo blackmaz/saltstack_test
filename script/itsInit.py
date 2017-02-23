212a1e8217c2880a28cb0c5d1af4f99e07f97128
CHO, Sungsic <blackmaz@users.noreply.github.com>
2017-02-23 10:00:18 +0900

    private key를 옵션으로 받도록 변경

--- script/itsInit.py
+++ script/itsInit.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python
 import salt.client.ssh.client
+import getopt
 
+def help():
+  print "python itsInit.py -p private key"
+  return
+
+ssh_key = ''
+
+def option():
+  grobal ssh_key
+  if len(sys.argv) is 1:
+    help()
+    sys.exit(1)
+  try:
+    options, args = getopt.getopt(sys.argv[1:], "p:h", ["priv=","help"])
+  except getopt.GetoptError as err:
+    print str(err)
+    help()
+    sys.exit(1)
+    
+  for opt, arg in options:
+    if (opt == '-p') or (opt == '--priv'):
+      ssh_key = arg
+    elif (opt == '-h') or (opt == '--help'):
+      help()
+      sys.exit(1)
+    return
+    
 #ssh_key='/home/ubuntu/aws/test_key_pair'
-ssh_key='/home/sungsic/.ssh/id_rsa'
+#ssh_key='/home/sungsic/.ssh/id_rsa'
+option()
 
 s = salt.client.ssh.client.SSHClient()
 r = s.cmd('*','test.ping',ssh_priv=ssh_key,ssh_identities_only=True)
